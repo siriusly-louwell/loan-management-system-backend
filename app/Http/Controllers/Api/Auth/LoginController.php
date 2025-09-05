40b4ac02387eb52e6981a8f41120d94206b36bf1
siriusly-louwell <bernaltelouwell23@gmail.com>
2025-09-05 10:14:46 +0800

    commit

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
